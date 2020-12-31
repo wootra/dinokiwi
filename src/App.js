9124fa583b9bcc0f071a3673c4bbfb9ea89314e6
Songhyeon Jun <shjeon0730@gmail.com>
2020-12-30 23:33:23 -0600

    temp page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import farm from './farm_img.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={farm} alt="성지키위농장 설명" />
       </header>
+      <content>
+        <p>공사중</p>
+        <div>
+          판매처 :{' '}
+          <a href="http://www.edinomall.com/shop/goods/goods_search.php?searched=Y&log=1&skey=all&hid_pr_text=&hid_link_url=&edit=&sword=%B0%F8%B7%E6%B3%AA%B6%F3%C2%FC%B4%D9%B7%A1%B3%F3%C0%E5&x=33&y=27">
+            공룡나라쇼핑몰
+          </a>
+        </div>
+      </content>
     </div>
   );
 }
