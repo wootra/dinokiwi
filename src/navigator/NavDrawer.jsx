bbf7fa8b38fc3f805e7e715129b9613c011d36b9
Songhyeon Jun <shjeon0730@gmail.com>
2021-01-11 19:04:39 -0600

    clean up

--- src/navigator/NavDrawer.jsx
+++ src/navigator/NavDrawer.jsx
@@ -4,7 +4,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
@@ -71,15 +70,6 @@ export default function NavDrawer(props) {
           <ListItem button key={item.text} onClick={e=>gotoUrl(item.ref)}>
             <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
             <ListItemText primary={item.text} />
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-            <ListItemText primary={text} />
           </ListItem>
         ))}
       </List>
